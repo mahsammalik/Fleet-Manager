912fa851b90c1a324e4de91a8400958b41c37347
Mahsam Iftikhar <malik_mahsam@hotmail.com>
2026-04-18 03:41:52 +0500

    MAIN :: BASE UTR EXPORT FIX

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "../store/authStore";
 
-const apiBaseURL =
+export const apiBaseURL =
   import.meta.env.VITE_API_URL ?? "http://103.131.215.13:4100/api";
 
 export const api = axios.create({
